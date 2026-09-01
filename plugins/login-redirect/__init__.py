a4c3122414d227905e2f773a87dbe7ebef8e465d
23younesm <64438560+23younesm@users.noreply.github.com>
2026-09-01 14:32:37 -0400

    Update redirect logic to handle both /login and /register paths for SSO

--- plugins/login-redirect/__init__.py
+++ plugins/login-redirect/__init__.py
@@ -1,12 +1,13 @@
-""" Auto-redirect /login to the SSO provider login """
+""" Auto-redirect /login and /register to the SSO provider login """
 
 from flask import redirect, request
 
 SSO_CLIENT_ID = 2
+REDIRECT_PATHS = ("/login", "/register")
 
 
 def load(app):
     @app.before_request
     def redirect_login_to_sso():
-        if request.method == "GET" and request.path == "/login":
+        if request.method == "GET" and request.path in REDIRECT_PATHS:
             return redirect("/sso/login/{}".format(SSO_CLIENT_ID))
